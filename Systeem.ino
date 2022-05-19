28fb70a3c644cb5dd36bc937576c1629895dbdce
XxJohnySeLlahxX <94017972+XxJohnySeLlahxX@users.noreply.github.com>
2022-05-19 19:57:28 +0200

    ad

---
+++ Systeem.ino
@@ -0,0 +1,36 @@
+const int N = 0;
+const int O = 1;
+const int Z = 2;
+const int W = 3;
+
+int laatsteRichting;
+
+boolean treinGedetecteerd() {
+  if (isClicked(W)) {
+    laatsteRichting = W;
+  } else if (isClicked(O)) {
+    laatsteRichting = O;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+boolean treinGedetecteerdOversteken() {
+  if (getbutton_West_Click()) {
+    laatsteRichting = W;
+  } else if (getbutton_Oost_Click()) {
+    laatsteRichting = O;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+boolean treinGepasseerd() {
+  return (isClicked(W) && laatsteRichting == O) || (isClicked(O) && laatsteRichting == W);
+}
+
+boolean ontruimingstijdVerlopen(unsigned long timer, unsigned long interval) {
+  return timerIsPassed(timer, interval);
+}
